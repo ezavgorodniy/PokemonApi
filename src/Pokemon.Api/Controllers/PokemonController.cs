fc95cd62ecbe3af49b94d8a9ab9d07a59ad4d822
Yevhen Zavhorodnii <Yevhen.Zavhorodnii@ivanti.com>
2020-03-13 23:35:39 +0000

    Prepare structure for the code

---
+++ src/Pokemon.Api/Controllers/PokemonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pokemon.Api.Models;
+
+namespace Pokemon.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{name}")]
+        public PokemonDescription Pokemon(string name)
+        {
+            return new PokemonDescription
+            {
+                Description = $"Description for {name}", 
+                Name = name
+            };
+        }
+    }
+}
---
+++ src/Pokemon.Api/Models/PokemonDescription.cs
@@ -0,0 +1,9 @@
+﻿namespace Pokemon.Api.Models
+{
+    public class PokemonDescription
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
